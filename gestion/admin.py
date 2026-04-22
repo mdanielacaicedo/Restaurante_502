5263b0691490fc9f32ede6587bf6d8ff1ebbf0f9
Daniela <mdanielacaicedo@ucundinamarca.edu.co>
2026-04-22 17:08:23 -0500

    primer commit

---
+++ gestion/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Cliente, Empleado, Mesa, Plato, Orden, DetalleOrden, Factura
+admin.site.register(Cliente,)    
+admin.site.register(Empleado,)    
+admin.site.register(Mesa,)    
+admin.site.register(Plato,)    
+admin.site.register(Orden,)    
+admin.site.register(DetalleOrden,)    
+admin.site.register(Factura,)    
+
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
+# Register your models here.
